688b907ecc476dbad9dd66c42c52ece61332bcb1
Mr Psycho <67461694+mr-abhi0@users.noreply.github.com>
2022-07-21 15:31:42 +0530

    Update key-gen.py

--- key-gen.py
+++ key-gen.py
@@ -1,9 +1,14 @@
 # import required module
 from cryptography.fernet import Fernet
 
+# take path of file as a input
+keyfile = input(r'Enter file name to save your key : ')
+
 # key generation
 key = Fernet.generate_key()
 
 # string the key in a file
-with open('filekey.key', 'wb') as filekey:
+with open(keyfile, 'wb') as filekey:
     filekey.write(key)
+
+    print('key file generated')
