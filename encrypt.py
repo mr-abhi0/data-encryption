78614cfbece4e2f6f4ef873c01585eb0403d09e8
Mr Psycho <67461694+mr-abhi0@users.noreply.github.com>
2022-07-21 15:20:38 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,21 +1,30 @@
 # import required module
 from cryptography.fernet import Fernet
 
+# take path of file as a input
+keyfile = input(r'Enter key file : ')
+
+# take path of file as a input
+filepath = input(r'Enter path of encryption file : ')
+
 # opening the key
-with open('filekey.key', 'rb') as filekey:
+with open(keyfile, 'rb') as filekey:
     key = filekey.read()
 
 # using the generated key
 fernet = Fernet(key)
 
 # opening the original file to encrypt
-with open('rzp.csv', 'rb') as file:
+with open(filepath, 'rb') as file:
     original = file.read()
 
 # encrypting the file
 encrypted = fernet.encrypt(original)
 
+
 # opening the file in write mode and
 # writing the encrypted data
-with open('rzp.csv', 'wb') as encrypted_file:
+with open(filepath, 'wb') as encrypted_file:
     encrypted_file.write(encrypted)
+
+    print('Encryption Done...')
