25cf6622f8ff8749d2a22100837a30eb42394768
Mr Psycho <67461694+mr-abhi0@users.noreply.github.com>
2022-07-21 15:19:57 +0530

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,15 +1,21 @@
 # import required module
 from cryptography.fernet import Fernet
 
+# take path of file as a input
+keyfile = input(r'Enter key file : ')
+
+# take path of file as a input
+filepath = input(r'Enter path of encryption file : ')
+
 # opening the key
-with open('filekey.key', 'rb') as filekey:
+with open(keyfile, 'rb') as filekey:
     key = filekey.read()
 
 # using the key
 fernet = Fernet(key)
 
 # opening the encrypted file
-with open('rzp.csv', 'rb') as enc_file:
+with open(filepath, 'rb') as enc_file:
     encrypted = enc_file.read()
 
 # decrypting the file
@@ -17,5 +23,8 @@ decrypted = fernet.decrypt(encrypted)
 
 # opening the file in write mode and
 # writing the decrypted data
-with open('rzp.csv', 'wb') as dec_file:
+with open(filepath, 'wb') as dec_file:
     dec_file.write(decrypted)
+
+
+print('Decryption Done...')
